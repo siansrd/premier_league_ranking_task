b054d841a8f45ea7e9692c9414ba3f99e871af52
siansrd <sianrobinsondavies@gmail.com>
2019-05-05 15:28:11 +0100

    Create generateTeamList function

--- models/ranking_calculator.js
+++ models/ranking_calculator.js
@@ -1,5 +1,13 @@
 const rankingCalculator = {
 
+  generateTeamList(matches) {
+    return matches.reduce((list, {team1, team2}) => {
+      list[team1.key] = { name: team1.name };
+      list[team2.key] = { name: team2.name };
+      return list;
+    }, {})
+  }
+
 }
 
 module.exports = rankingCalculator;
--- models/specs/ranking_calculator.test.js
+++ models/specs/ranking_calculator.test.js
@@ -1,9 +1,38 @@
 const calc = require('../ranking_calculator.js');
+const leageData = require('../../data/16_17_league_data.json')
 
 describe('Ranking Calculator', () => {
 
   test('calculator exists', () => {
     expect(calc).toBeTruthy();
+  })
+
+  test('generate unique team list', () => {
+    const teamList = {
+      hull: { name: 'Hull City' }, 
+      leicester: { name: 'Leicester City' }, 
+      burnley: { name: 'Burnley' },
+      swansea: { name: 'Swansea' },
+      crystalpalace: { name: 'Crystal Palace' },
+      westbrom: { name: 'West Bromwich Albion' },
+      everton: { name: 'Everton' },
+      tottenham: { name: 'Tottenham Hotspur' },
+      middlesbrough: { name: 'Middlesbrough' },
+      stoke: { name: 'Stoke City' },
+      southampton: { name: 'Southampton' },
+      watford: { name: 'Watford' },
+      mancity: { name: 'Manchester City' },
+      sunderland: { name: 'Sunderland' },
+      bournemouth: { name: 'Bournemouth' },
+      manutd: { name: 'Manchester United' },
+      arsenal: { name: 'Arsenal' },
+      liverpool: { name: 'Liverpool' },
+      chelsea: { name: 'Chelsea'},
+      westham: { name: 'West Ham United' }
+    }
+
+    const matchDay1 = leageData.rounds[0].matches;
+    expect(calc.generateTeamList(matchDay1)).toEqual(teamList);
   })
 
 });
