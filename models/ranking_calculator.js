652b9279562c87b8819b4357c1c1f74109b327b4
siansrd <sianrobinsondavies@gmail.com>
2019-05-05 14:25:37 +0100

    Initital test to check calculator has been created

---
+++ models/ranking_calculator.js
@@ -0,0 +1,5 @@
+const rankingCalculator = {
+
+}
+
+module.exports = rankingCalculator;
---
+++ models/specs/ranking_calculator.test.js
@@ -0,0 +1,9 @@
+const calc = require('../ranking_calculator.js');
+
+describe('Ranking Calculator', () => {
+
+  test('object exists', () => {
+    expect(calc).toBeTruthy();
+  })
+
+});
