100f804d0c9c5cf14addcf3bcd718ad944f9da5b
siansrd <sianrobinsondavies@gmail.com>
2019-05-05 16:19:36 +0100

    Refactor to generate all team fields

--- models/ranking_calculator.js
+++ models/ranking_calculator.js
@@ -2,8 +2,8 @@ const rankingCalculator = {
 
   generateTeamList(matches) {
     return matches.reduce((list, {team1, team2}) => {
-      list[team1.key] = { name: team1.name };
-      list[team2.key] = { name: team2.name };
+      list[team1.key] = { name: team1.name, wins: 0, losses: 0, goalsFor: 0, goalsAgainst: 0, goalDifference: 0, points: 0 };
+      list[team2.key] = { name: team2.name, wins: 0, losses: 0, goalsFor: 0, goalsAgainst: 0, goalDifference: 0, points: 0 };
       return list;
     }, {})
   }
--- models/specs/ranking_calculator.test.js
+++ models/specs/ranking_calculator.test.js
@@ -9,30 +9,32 @@ describe('Ranking Calculator', () => {
 
   test('generate unique team list', () => {
     const teamList = {
-      hull: { name: 'Hull City' }, 
-      leicester: { name: 'Leicester City' }, 
-      burnley: { name: 'Burnley' },
-      swansea: { name: 'Swansea' },
-      crystalpalace: { name: 'Crystal Palace' },
-      westbrom: { name: 'West Bromwich Albion' },
-      everton: { name: 'Everton' },
-      tottenham: { name: 'Tottenham Hotspur' },
-      middlesbrough: { name: 'Middlesbrough' },
-      stoke: { name: 'Stoke City' },
-      southampton: { name: 'Southampton' },
-      watford: { name: 'Watford' },
-      mancity: { name: 'Manchester City' },
-      sunderland: { name: 'Sunderland' },
-      bournemouth: { name: 'Bournemouth' },
-      manutd: { name: 'Manchester United' },
-      arsenal: { name: 'Arsenal' },
-      liverpool: { name: 'Liverpool' },
-      chelsea: { name: 'Chelsea'},
-      westham: { name: 'West Ham United' }
+      hull: { name: 'Hull City', wins: 0, losses: 0, goalsFor: 0, goalsAgainst: 0, goalDifference: 0, points: 0 }, 
+      leicester: { name: 'Leicester City', wins: 0, losses: 0, goalsFor: 0, goalsAgainst: 0, goalDifference: 0, points: 0 }, 
+      burnley: { name: 'Burnley', wins: 0, losses: 0, goalsFor: 0, goalsAgainst: 0, goalDifference: 0, points: 0 },
+      swansea: { name: 'Swansea', wins: 0, losses: 0, goalsFor: 0, goalsAgainst: 0, goalDifference: 0, points: 0 },
+      crystalpalace: { name: 'Crystal Palace', wins: 0, losses: 0, goalsFor: 0, goalsAgainst: 0, goalDifference: 0, points: 0 },
+      westbrom: { name: 'West Bromwich Albion', wins: 0, losses: 0, goalsFor: 0, goalsAgainst: 0, goalDifference: 0, points: 0 },
+      everton: { name: 'Everton', wins: 0, losses: 0, goalsFor: 0, goalsAgainst: 0, goalDifference: 0, points: 0 },
+      tottenham: { name: 'Tottenham Hotspur', wins: 0, losses: 0, goalsFor: 0, goalsAgainst: 0, goalDifference: 0, points: 0 },
+      middlesbrough: { name: 'Middlesbrough', wins: 0, losses: 0, goalsFor: 0, goalsAgainst: 0, goalDifference: 0, points: 0 },
+      stoke: { name: 'Stoke City', wins: 0, losses: 0, goalsFor: 0, goalsAgainst: 0, goalDifference: 0, points: 0 },
+      southampton: { name: 'Southampton', wins: 0, losses: 0, goalsFor: 0, goalsAgainst: 0, goalDifference: 0, points: 0 },
+      watford: { name: 'Watford', wins: 0, losses: 0, goalsFor: 0, goalsAgainst: 0, goalDifference: 0, points: 0 },
+      mancity: { name: 'Manchester City', wins: 0, losses: 0, goalsFor: 0, goalsAgainst: 0, goalDifference: 0, points: 0 },
+      sunderland: { name: 'Sunderland', wins: 0, losses: 0, goalsFor: 0, goalsAgainst: 0, goalDifference: 0, points: 0 },
+      bournemouth: { name: 'Bournemouth', wins: 0, losses: 0, goalsFor: 0, goalsAgainst: 0, goalDifference: 0, points: 0 },
+      manutd: { name: 'Manchester United', wins: 0, losses: 0, goalsFor: 0, goalsAgainst: 0, goalDifference: 0, points: 0 },
+      arsenal: { name: 'Arsenal', wins: 0, losses: 0, goalsFor: 0, goalsAgainst: 0, goalDifference: 0, points: 0 },
+      liverpool: { name: 'Liverpool', wins: 0, losses: 0, goalsFor: 0, goalsAgainst: 0, goalDifference: 0, points: 0 },
+      chelsea: { name: 'Chelsea', wins: 0, losses: 0, goalsFor: 0, goalsAgainst: 0, goalDifference: 0, points: 0 },
+      westham: { name: 'West Ham United', wins: 0, losses: 0, goalsFor: 0, goalsAgainst: 0, goalDifference: 0, points: 0 }
     }
 
     const matchDay1 = leageData.rounds[0].matches;
     expect(calc.generateTeamList(matchDay1)).toEqual(teamList);
   });
+
+
 
 });
