b1a73fa460ae9070373f7deaa34e16b529ae5218
siansrd <sianrobinsondavies@gmail.com>
2019-05-05 14:58:33 +0100

    Rename test

--- models/specs/ranking_calculator.test.js
+++ models/specs/ranking_calculator.test.js
@@ -2,7 +2,7 @@ const calc = require('../ranking_calculator.js');
 
 describe('Ranking Calculator', () => {
 
-  test('object exists', () => {
+  test('calculator exists', () => {
     expect(calc).toBeTruthy();
   })
 
