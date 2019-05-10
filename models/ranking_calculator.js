7071511000e3059628406d0a2de75d7eaae182f3
siansrd <sianrobinsondavies@gmail.com>
2019-05-10 15:43:35 +0100

    Delete unused line

--- models/ranking_calculator.js
+++ models/ranking_calculator.js
@@ -117,7 +117,6 @@ const rankingCalculator = {
   },
 
   sortTeams(teamsList) {
-    const teamKeys = this.getTeamKeys(teamsList);
     const sortedKeys = this.getSortedTeamKeys(teamsList);
     
     return sortedKeys.map((teamKey, index) => {
